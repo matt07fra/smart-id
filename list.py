a8803c98a39ebaa48938c92d061466836b590fb4
matt07fra <mathew.francis@indiumsoft.com>
2022-07-07 17:32:25 +0530

    commit list

---
+++ list.py
@@ -0,0 +1,60 @@
+#python loops
+'''fruits =['apple','grapes','cherry']
+for x in fruits:
+    print(x)'''
+
+#looping through strings
+
+'''for x in 'apple':
+    print(x)'''
+
+#break
+'''fruits =['apple','grapes','cherry']
+for x in fruits:
+    print(x)
+    if x=='grapes':
+        break'''
+
+'''fruits =['apple','grapes','cherry']
+for x in fruits:
+    if x=='grapes':
+        break
+    print(x)'''
+
+
+'''fruits =['apple','grapes','cherry']
+for x in fruits:
+    print(x)
+    if x=='grapes':
+        break'''
+
+#continue
+
+'''fruits =['apple','grapes','cherry']
+for x in fruits:
+    if x=='grapes':
+        continue
+    print(x)'''
+
+#range() function
+'''for x in range(6):
+    print(x)'''
+'''for x in range(2,6):
+    print(x)'''
+
+#else
+'''for x in range(6):
+    print(x)
+else:
+    print("finally finished")'''
+
+'''for x in range(6):
+    if x==2:
+        break
+    print(x)
+else:
+    print("finally finished")'''
+
+empty=[i if i=='m' break for i in 'Indium software']
+
+
